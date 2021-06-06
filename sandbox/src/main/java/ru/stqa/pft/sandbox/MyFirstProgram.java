3d6622c401474a382c9c25f35e1d19dcff0dcd83
ArcanaWizard <arcana.wizard@gmail.com>
2021-06-06 19:58:06 +0500

    Задание №2, пункты 1-2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,28 +3,14 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Ruslan");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Point p1 = new Point(2, 2);
+    Point p2 = new Point(3, 1);
+    System.out.println("Расстояние между точками = " + distance(p1, p2));
 
-    double a = 4;
-    double b =6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hell " + somebody);
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) + Math.pow(p1.getY() - p2.getY(), 2));
   }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
-
 }
