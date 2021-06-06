25fee874834dd2fa878f4ed1dc0219d8e7e5b873
ArcanaWizard <arcana.wizard@gmail.com>
2021-06-06 20:14:56 +0500

    Задание №2 пункты 3-4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,13 +4,7 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(2, 2);
-    Point p2 = new Point(3, 1);
-    System.out.println("Расстояние между точками = " + distance(p1, p2));
-
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) + Math.pow(p1.getY() - p2.getY(), 2));
+    Point p = new Point(2, 2, 3, 1);
+    System.out.println("Расстояние между точками = " + p.distance());
   }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,17 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  private double x , y;
+  private double x1, y1, x2, y2;
 
-  public Point(double x, double y){
-    this.x = x;
-    this.y = y;
+  public Point(double x1, double y1, double x2, double y2){
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
   }
-  public double getX(){
-    return x;
-  }
-
-  public double getY(){
-    return y;
+  public double distance() {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
   }
 }
