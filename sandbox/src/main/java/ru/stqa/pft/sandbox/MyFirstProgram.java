4797f1847014beddce6a1b30657415b859d105ec
ArcanaWizard <arcana.wizard@gmail.com>
2021-06-06 15:28:49 +0500

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,10 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	
-	
+
+
   public static void main(String[] args) {
-   System.out.println("Hell world");
+    System.out.println("Hell world");
   }
-  
+
 }
