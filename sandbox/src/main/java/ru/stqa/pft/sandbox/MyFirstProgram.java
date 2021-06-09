97ec1ce783ad8ced5217bf40ba62ed508478cce1
ArcanaWizard <arcana.wizard@gmail.com>
2021-06-09 22:07:54 +0500

    Доработка класса Point по замечаниям

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,15 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    Point p = new Point(2, 2, 3, 1);
-    System.out.println("Расстояние между точками = " + p.distance());
+   Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+   Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p1 = new Point(2, 2);
+    Point p2 = new Point(3, 1);
+    System.out.println("Расстояние между точками = " + p1.distanceTo(p2));
   }
+
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,15 +1,18 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  private double x1, y1, x2, y2;
+  private final double x;
+  private final double y;
 
-  public Point(double x1, double y1, double x2, double y2){
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
   }
-  public double distance() {
-    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+
+  public double distanceTo(Point other) {
+    double dx = other.x - this.x;
+    double dy = other.y - this.y;
+    double distance = Math.sqrt(dx * dx + dy * dy);
+    return distance;
   }
 }
